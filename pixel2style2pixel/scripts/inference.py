b446533ebab001ab1ebd110cbed79e0cce03fb0c
yochimonji <yochimonji@gmail.com>
2021-09-27 03:15:32 +0900

    add 画像を読み込んで変換してcv2で表示した

--- pixel2style2pixel/scripts/inference.py
+++ pixel2style2pixel/scripts/inference.py
@@ -1,20 +1,17 @@
 import os
 from argparse import Namespace
 
-from tqdm import tqdm
-import time
 import numpy as np
 import torch
-from PIL import Image
 from torch.utils.data import DataLoader
 import sys
+import cv2
 
 sys.path.append(".")
 sys.path.append("..")
 
 from configs import data_configs
 from datasets.inference_dataset import InferenceDataset
-from utils.common import tensor2im, log_input_image
 from options.test_options import TestOptions
 from models.psp import pSp
 
@@ -25,16 +22,6 @@ def run():
     if test_opts.resize_factors is not None:
         assert len(
             test_opts.resize_factors.split(',')) == 1, "When running inference, provide a single downsampling factor!"
-        out_path_results = os.path.join(test_opts.exp_dir, 'inference_results',
-                                        'downsampling_{}'.format(test_opts.resize_factors))
-        out_path_coupled = os.path.join(test_opts.exp_dir, 'inference_coupled',
-                                        'downsampling_{}'.format(test_opts.resize_factors))
-    else:
-        out_path_results = os.path.join(test_opts.exp_dir, 'inference_results')
-        out_path_coupled = os.path.join(test_opts.exp_dir, 'inference_coupled')
-
-    os.makedirs(out_path_results, exist_ok=True)
-    os.makedirs(out_path_coupled, exist_ok=True)
 
     # update test options with options used during training
     ckpt = torch.load(test_opts.checkpoint_path, map_location='cpu')
@@ -50,7 +37,6 @@ def run():
     net.eval()
     net.cuda()
 
-    print('Loading dataset for {}'.format(opts.dataset_type))
     dataset_args = data_configs.DATASETS[opts.dataset_type]
     transforms_dict = dataset_args['transforms'](opts).get_transforms()
     dataset = InferenceDataset(root=opts.data_path,
@@ -65,48 +51,14 @@ def run():
     if opts.n_images is None:
         opts.n_images = len(dataset)
 
-    global_i = 0
-    global_time = []
-    for input_batch in tqdm(dataloader):
-        if global_i >= opts.n_images:
-            break
+    for input_batch in dataloader:
         with torch.no_grad():
             input_cuda = input_batch.cuda().float()
-            tic = time.time()
             result_batch = run_on_batch(input_cuda, net, opts)
-            toc = time.time()
-            global_time.append(toc - tic)
-
-        for i in range(opts.test_batch_size):
-            result = tensor2im(result_batch[i])
-            im_path = dataset.paths[global_i]
-
-            if opts.couple_outputs or global_i % 100 == 0:
-                input_im = log_input_image(input_batch[i], opts)
-                resize_amount = (256, 256) if opts.resize_outputs else (opts.output_size, opts.output_size)
-                if opts.resize_factors is not None:
-                    # for super resolution, save the original, down-sampled, and output
-                    source = Image.open(im_path)
-                    res = np.concatenate([np.array(source.resize(resize_amount)),
-                                          np.array(input_im.resize(resize_amount, resample=Image.NEAREST)),
-                                          np.array(result.resize(resize_amount))], axis=1)
-                else:
-                    # otherwise, save the original and output
-                    res = np.concatenate([np.array(input_im.resize(resize_amount)),
-                                          np.array(result.resize(resize_amount))], axis=1)
-                Image.fromarray(res).save(os.path.join(out_path_coupled, os.path.basename(im_path)))
-
-            im_save_path = os.path.join(out_path_results, os.path.basename(im_path))
-            Image.fromarray(np.array(result)).save(im_save_path)
-
-            global_i += 1
-
-    stats_path = os.path.join(opts.exp_dir, 'stats.txt')
-    result_str = 'Runtime {:.4f}+-{:.4f}'.format(np.mean(global_time), np.std(global_time))
-    print(result_str)
-
-    with open(stats_path, 'w') as f:
-        f.write(result_str)
+            result = tensor2cvimg(result_batch[0])
+            cv2.imshow('Image', result)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
 
 
 def run_on_batch(inputs, net, opts):
@@ -130,6 +82,16 @@ def run_on_batch(inputs, net, opts):
             result_batch.append(res)
         result_batch = torch.cat(result_batch, dim=0)
     return result_batch
+
+
+def tensor2cvimg(var):
+    var = var.cpu().detach().transpose(0, 2).transpose(0, 1).numpy().copy()
+    var = cv2.cvtColor(var, cv2.COLOR_BGR2RGB)
+    var = ((var + 1) / 2)
+    var[var < 0] = 0
+    var[var > 1] = 1
+    var = var * 255
+    return var.astype('uint8')
 
 
 if __name__ == '__main__':
