17e6d89671819dcecc3232c314a916d172aeae68
yochimonji <yochimonji@gmail.com>
2021-09-26 22:09:57 +0900

    add　ボタンを押したら表示

--- main.py
+++ main.py
@@ -1,15 +1,49 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.image import Image
+from kivy.graphics.texture import Texture
+from kivy.clock import Clock
+from kivy.uix.behaviors import ButtonBehavior
+from kivy.properties import ObjectProperty
+import cv2
 
 
-class PongGame(Widget):
+# 撮影ボタン
+class ImageButton(ButtonBehavior, Image):
+    preview = ObjectProperty(None)
+
+    # ボタンを押したときに実行
+    def on_press(self):
+        cv2.namedWindow("CV2 Image")
+        cv2.imshow("CV2 Image", self.preview.frame)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+class CameraPreview(Image):
+    def __init__(self, **kwargs):
+        super(CameraPreview, self).__init__(**kwargs)
+        # 0番目のカメラに接続
+        self.capture = cv2.VideoCapture(0)
+        # 描画のインターバルを設定
+        Clock.schedule_interval(self.update, 1.0 / 30)
+
+    # インターバルで実行する描画メソッド
+    def update(self, dt):
+        # フレームを読み込み
+        ret, self.frame = self.capture.read()
+        # Kivy Textureに変換
+        buf = cv2.flip(self.frame, 0).tostring()
+        texture = Texture.create(size=(self.frame.shape[1], self.frame.shape[0]), colorfmt='bgr') 
+        texture.blit_buffer(buf, colorfmt='bgr', bufferfmt='ubyte')
+        # インスタンスのtextureを変更
+        self.texture = texture
+
+class MainScreen(Widget):
     pass
 
-
-class PongApp(App):
+class MyCameraApp(App):
     def build(self):
-        return PongGame()
-
+        return MainScreen()
 
 if __name__ == '__main__':
-    PongApp().run()
+    MyCameraApp().run()
